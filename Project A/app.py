a9accc111f9c4263e4d9213f97f4a4d8d8e84a3f
“romanooka <l2revolt1@gmail.com>
2024-04-17 14:48:08 +0700

    Weekly_theory

---
+++ Project A/app.py
@@ -0,0 +1,21 @@
+import dev
+
+
+
+
+def tambah(a,b):
+    return a+b
+
+
+hasil =  dev.getUser(nama='budi', umur='30', kota='jakarta')
+print(hasil)
+
+
+from dev import getUser
+
+
+from PakageA.module1 import getUser
+
+
+hasil14 = getUser('john', '25', 'jakarta'.)
+print(hasil14)
---
+++ Project A/dev.py
@@ -0,0 +1,6 @@
+# module dev
+
+def getUser(**kwargs):
+    return kwargs
+
+getUser(nama='saya', umur=20, kota="katik")
